56096b41119c6e1c64ba702b9c07646e360c5ae1
agstephens <ag.stephens@stfc.ac.uk>
2022-12-12 21:44:41 +0000

    Updated command line options

--- kerchunk_tools/cli.py
+++ kerchunk_tools/cli.py
@@ -24,22 +24,6 @@ def main():
     return 0
 
 
-"""
-@main.command()
-@click.argument("file_path")
-@click.option("-m", "--mappings", default=None)
-@click.option("-r", "--rules", default=None)
-@click.option("-i", "--ignore-attrs", default=None)
-@click.option("-G", "--ignore-all-globals", default=False)
-@click.option("-D", "--ignore-all-dimensions", default=False)
-@click.option("-V", "--ignore-all-variables", default=False)
-@click.option("-A", "--ignore-all-variable-attrs", default=False)
-@click.option("--auto-cache/--no-auto-cache", default=False)
-@click.option("-l", "--log-mode", default="standard")
-@click.option("-v", "--verbose/--no-verbose", default=False)
-@click.option("-t", "--template", default="auto")
-"""
-
 def parse_s3_config_file(fpath):
     return json.load(open(fpath))
 
@@ -49,20 +33,15 @@ def parse_s3_config_file(fpath):
 @click.option("-p", "--prefix", default=DEFAULTS["prefix"])
 @click.option("-o", "--output-path", default=DEFAULTS["output_path"])
 @click.option("-b", "--max-bytes", default=DEFAULTS["max_bytes"])
+@click.option("-c", "--s3-config-file", default=None)
 def create(file_uris, file_uris_file=None, prefix=DEFAULTS["prefix"], 
            output_path=DEFAULTS["output_path"], max_bytes=DEFAULTS["max_bytes"],
            s3_config_file=None):
-#def create(is3_config=None, max_bytes=-1),file_uris, prefix, output_path="index.json", max_bytes=-1) 
-
-#def create(file_path, mappings=None, rules=None, ignore_attrs=None, ignore_all_globals=False,
-#          ignore_all_dimensions=False, ignore_all_variables=False, ignore_all_variable_attrs=False,
-#          auto_cache=False, log_mode="standard", verbose=False, template="auto"):
-
     s3_config = parse_s3_config_file(s3_config_file) if s3_config_file else None
     if file_uris and file_uris_file:
         raise ValueError(f"Tool does not support setting BOTH 'file_uris' and 'file_uris_file' options")
 
-    file_uris = open(file_uris_file).strip().split() if file_uris_file else file_uris
+    file_uris = open(file_uris_file).read().strip().split() if file_uris_file else file_uris
 
     indexer = kct.indexer.Indexer(s3_config=s3_config, max_bytes=max_bytes)
     indexer.create(file_uris, prefix, output_path=output_path, max_bytes=max_bytes)
