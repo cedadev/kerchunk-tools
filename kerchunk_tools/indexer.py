75c59fe64215494c197b586394f5a80bead2f115
William Cross <wcross@host425.jc.rl.ac.uk>
2023-02-20 13:26:46 +0000

    added a way to decode and encode base64 numbers

--- kerchunk_tools/indexer.py
+++ kerchunk_tools/indexer.py
@@ -3,22 +3,155 @@ import json
 import fsspec
 import kerchunk.hdf
 from kerchunk.combine import MultiZarrToZarr
+import re
+import base64
+import struct
+import cftime
 
 from urllib.parse import urlparse
 
 from .utils import prepare_dir           #had a . before the word before import
 
+_cached_ref_time = {}
 
-def normalise_datetimes(content, time_attrs):
-    print(content, "content")
-    print(time_attrs, "time_attrs")
-    #print("HI", type(x), list(x.keys()))
-    #navigate to JSON file to find key/values that represent the time attriburestime values and potentially any bounds that relate to time (as linked via the attribute: time:bounds)
+def _pid():
+    return os.getpid()
+
+def _set_ref_time(t):
+    _cached_ref_time[_pid()] = t
+
+def _get_ref_time():
+    return _cached_ref_time[_pid()]
+
+def _clear_ref_time():
+    del _cached_ref_time[_pid()]
+
+
     
-#def normalise_datetimes(times, frm_units, frm_calendar, to_units, to_calendar):
-#    import cftime
-#    dates = cftime.num2date(times, frm_units, frm_calendar)
-#    return cftime.date2num(dates, to_units, to_calendar)
+def normalise_datetimes(var):
+    import datetime
+    ref_time = _get_ref_time()
+    time_origin = ref_time["time_origin"]
+    
+    d3=var["time/.zattrs"]
+            
+    res = d3.split('\n    "units": ', 1)
+    new_string = res[1]
+    result = re.search('"days since (.*)"', new_string)
+    date=(result.group(1))
+    
+    time_origin_short = (date.split())[0]
+    date_short = (date.split())[0]
+    
+    print(d3)
+    
+    time_origin_date = datetime.datetime.strptime(time_origin,"%Y-%m-%d %H:%M:%S")
+    date_date = datetime.datetime.strptime(date,"%Y-%m-%d %H:%M:%S")
+    
+    delta = date_date - time_origin_date
+    
+    days_difference = delta.days
+
+    print(days_difference)
+
+
+    convert_times(d3, ref_time)
+
+    
+    lis=[]
+    sub="time/"
+    v_time=[s for s in var if sub in s]
+    try:
+        v_time.remove("time/.zarray")
+    except:
+        pass
+    try:
+        v_time.remove("time/.zattrs")
+    except:
+        pass
+    
+    for i in v_time:
+        val=(var[i])
+        this=(decode_base_64(val, sub))
+        lis.append(this)
+        
+        
+    lis2=[]
+    sub="time_bounds/"
+    v_bound=[s for s in var if "time_bounds/" in s]
+    try:
+        v_bound.remove("time_bounds/.zarray")
+    except:
+        pass
+    try:
+        v_bound.remove("time_bounds/.zattrs")
+    except:
+        pass
+        
+    for i in v_bound:
+        val=(var[i])
+        this=(decode_base_64(val, sub))
+        lis2.append(this)
+    
+    
+    #print(v_time)
+    #print(lis)
+    
+    #print(v_bound)
+    #print(lis2)
+    
+    
+    def convert_times(times, frm_units, frm_calendar, to_units, to_calendar):
+        dates = cftime.num2date(times, frm_units, frm_calendar)#gets datetime from int in calender time
+        return cftime.date2num(dates, to_units, to_calendar)#convrets back to into in other calender
+    
+    
+    #def date2num(info, date_from):
+    #days_since = date_from["time_origin"]
+    #units = ("days since", date_since)
+    #return cftime.date2num(date, units, calendar)
+    
+def encode_base_64(val, sub):
+    if sub == "time_bound":
+        val1 = val[0]
+        val2 = val[1]
+        v = struct.pack("dd", val1, val2)
+        end_val = (b"base64:" + base64.b64encode(v)).decode()
+        return(end_val)
+    
+    if sub == "time":
+        v = struct.pack("d", val)
+        end_val = (b"base64:" + base64.b64encode(v)).decode()
+        return(end_val)
+
+def decode_base_64(val, sub):
+    v = val[7:]
+    byte_list = base64.b64decode(v)
+    if sub == "time/":
+        val_list = struct.unpack("d", byte_list)          #https://docs.python.org/3/library/struct.html#format-characters
+        val = val_list[0]
+        if str(val).endswith('.0'):
+            temp_val = str(val)[:-2]
+            new_val = int(temp_val)
+        else:
+            new_val = val
+            
+        return(new_val)
+            
+    if sub == "time_bounds/":
+        val_list = struct.unpack("dd", byte_list)          #https://docs.python.org/3/library/struct.html#format-characters
+        end_list = []
+        for val in val_list:
+            if str(val).endswith('.0'):
+                temp_val = str(val)[:-2]
+                new_val = int(temp_val)
+                end_list.append(new_val)
+            else:
+                end_list.append(val)
+        return(end_list)
+    
+    
+    
     
 
 class Indexer:
@@ -39,14 +172,29 @@ class Indexer:
             self.scheme = "posix"
             self.uri_prefix = ""
             
-        if use_time == None:
-            self.use_time = None
-            
-        if use_time:
-            self.use_time = "exists"
+        self._prepare_time(ref_time_file=use_time)
 
         self.cache_dir = cache_dir
         self.update_max_bytes(max_bytes)
+        
+        
+    def _prepare_time(self, ref_time_file=None):
+        if not ref_time_file:
+            self.ref_time = None
+            return
+
+        from netCDF4 import Dataset
+        with Dataset(ref_time_file) as ds:
+
+            try:
+                time_var = ds.get_variables_by_attributes(standard_name="time")[0]
+                self.ref_time = {key: time_var.getncattr(key) for key in time_var.ncattrs()}
+                self.ref_time["name"] = time_var.name
+            except KeyError as exc:
+                raise Exception(f"Could not identify time coordinate in: {fpath}")
+            _set_ref_time(self.ref_time)
+        
+        
 
     def update_max_bytes(self, max_bytes):
         self.max_bytes = max_bytes if max_bytes > 0 else self.MAX_INDEXED_ARRAY_SIZE_IN_BYTES
@@ -69,10 +217,10 @@ class Indexer:
             kwargs["remote_protocol"] = "s3"
             kwargs["remote_options"] = self.fssopts
       
-        if self.use_time == "exists" and skip == False:
-            mzz = MultiZarrToZarr(singles, concat_dims=["time"], preprocess=normalise_datetimes, **kwargs)
+        if self.ref_time != None and skip == False:
+            mzz = MultiZarrToZarr(singles, concat_dims=["time"], preprocess=normalise_datetimes if self.ref_time else None, **kwargs)
         
-        if self.use_time == None or skip==None:
+        if self.ref_time == None or skip==None:
             mzz = MultiZarrToZarr(singles, concat_dims=["time"], **kwargs)
         
         return mzz.translate() 
@@ -104,8 +252,6 @@ class Indexer:
 
         # Decide JSON content
 
-        #print(single_indexes[0])####################### single_indexes shows the json of the thing
-        import re
         dates = None
         skip = None
         for item in range(len(file_uris)):
@@ -126,8 +272,8 @@ class Indexer:
             if dates == None:
                 dates = date
                 
-        if skip == False:
-            print("[INFO] THE DATES UNITS ARE DIFFERENT FOR SOME OF THE FILES IF YOU HAVEN'T DONE SO ALREADY YOU MAY WANT TO RUN WITH -t TO NORMALISE THE DATETIMES TO AVOID CONCATINATION ERRORS!!")
+        if skip == False and self.ref_time == None:
+            print("[INFO] THE DATES UNITS ARE DIFFERENT FOR SOME OF THE FILES, YOU MAY WANT TO RUN WITH -t TO NORMALISE THE DATETIMES TO AVOID CONCATINATION ERRORS!!")
 
 
         if len(file_uris) == 1:
@@ -149,6 +295,9 @@ class Indexer:
                 kc_file.write(json_to_write)
 
         print(f"[INFO] Written file: {output_uri}")
+        
+        if self.ref_time != None:
+            _clear_ref_time()
         return output_uri
 
 
